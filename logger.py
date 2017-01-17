c5fe198b6bb1bcb277da5e807f06604ad7aef204
Mares.Barekzai <Mares.Barekzai@lmu.de>
2017-01-17 20:48:26 +0100

    Added PlotTimeSeriesLogger

--- logger.py
+++ logger.py
@@ -64,6 +64,27 @@ class MultiLogger(object):
         for logger in self.loggers:
             logger.finalize()
 
+class PlotTimeSeriesLogger(object):
+    def __init__(self, quantities):
+        self.t = []
+        self.quantities = quantities
+        self.data = [[] for _ in quantities]
+
+    def log_state(self, state):
+        self.t.append(state.t)
+        for name, storage in zip(self.quantities, self.data):
+            storage.append(getattr(state, name))
+
+    def finalize(self):
+        import matplotlib.pyplot as plt
+        fig, axes = plt.subplots(len(self.quantities))
+        for name, storage, ax in zip(self.quantities, self.data, axes):
+            ax.plot(self.t, storage)
+            ax.set_ylabel(name)
+        axes[-1].set_xlabel('time')
+        fig.savefig('daten.png')
+
 LOGGERS = {
+    'MultiPlot': PlotTimeSeriesLogger,
     'Logger': Logger,
      }
