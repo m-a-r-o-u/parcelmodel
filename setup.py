db0d7cf7b8608918688f06af03ab064f1bf3cd5a
Mares.Barekzai <Mares.Barekzai@lmu.de>
2017-01-19 11:28:23 +0100

    Add explicit option to particle distribution

--- setup.py
+++ setup.py
@@ -7,14 +7,23 @@ def create_uniform_particle_distribution(total, groups):
     r_min = np.sort(np.random.uniform(1.e-6, 2.e-6, groups))
     return r_min, particle_count
 
+def create_explicit_particle_distribution(r_min, particle_count):
+    return r_min, particle_count
+
 PARTICLE_DISTRIBUTIONS = {
     'uniform': create_uniform_particle_distribution,
+    'explicit': create_explicit_particle_distribution,
     }
 
 def create_particle_distribution(definitions):
-    total = float(definitions['total'])
-    groups = int(definitions['groups'])
-    return PARTICLE_DISTRIBUTIONS[definitions['type']](total, groups)
+    if definitions['type'] == 'uniform':
+        total = float(definitions['total'])
+        groups = int(definitions['groups'])
+        return PARTICLE_DISTRIBUTIONS[definitions['type']](total, groups)
+    elif definitions['type'] == 'explicit':
+        r_min = map(float, definitions['r_min'])
+        particle_count = map(float, definitions['particle_count'])
+        return PARTICLE_DISTRIBUTIONS[definitions['type']](r_min, particle_count)
 
 def model_init(initial_conditions):
     r_min, particle_count = create_particle_distribution(initial_conditions['particle_distribution'])
