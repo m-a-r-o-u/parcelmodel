e2b1e7577cca8e69a35a4c52502c084c104bc8f5
Mares.Barekzai <Mares.Barekzai@lmu.de>
2017-01-20 14:28:23 +0100

    Adding logger_config adapter

--- box.py
+++ box.py
@@ -3,10 +3,13 @@ import yaml
 from setup import model_init
 from logger import logger_factory
 
+def logger_config(config):
+  return config['output']
+
 def main():
     config = yaml.load(sys.stdin)
     model = model_init(config['initial_conditions'])
-    with logger_factory(config['output']) as logger:
+    with logger_factory(logger_config(config)) as logger:
         model.run(logger)
 
 if __name__ == "__main__":
