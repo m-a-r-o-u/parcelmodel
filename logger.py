19412936b46aba75065cbc1c78befea681bd4393
Mares.Barekzai <Mares.Barekzai@lmu.de>
2017-01-23 16:12:45 +0100

    Added generate_kwargs to co. logger with. yamlargs

--- logger.py
+++ logger.py
@@ -1,10 +1,18 @@
 def logger_factory(config):
+    import inspect
     if isinstance(config, list):
         return MultiLogger([logger_factory(c) for c in config])
     else:
         constructor = LOGGERS[config['type']]
-        kwargs = {k:v for k,v in config.iteritems() if k != 'type'}
+        kwargs = generate_kwargs(constructor, config)
         return constructor(**kwargs)
+
+def generate_kwargs(constructor, config):
+    import inspect
+    inspected_args = [_i for _i in inspect.getargspec(constructor.__init__).args if not _i == 'self']
+    yaml_args = {k:v for k,v in config.iteritems() if k != 'type'}
+    kwargs = {i:yaml_args[i] for i in inspected_args if i in yaml_args}
+    return kwargs
 
 class BaseLogger(object):
     def set_units(self, units):
