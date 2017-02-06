32af317694c83e4fa69dc3200c4cf5e1b9c3ecb9
Mares.Barekzai <Mares.Barekzai@lmu.de>
2017-02-06 13:52:16 +0100

    Adding Argparse to read in config.yaml

--- box.py
+++ box.py
@@ -3,9 +3,11 @@ import yaml
 from setup import model_init
 from logger import logger_factory
 from config_tree import logger_config
+from my_argparse import argparse_init
 
 def main():
-    config = yaml.load(sys.stdin)
+    args = argparse_init()
+    config = yaml.load( open(args.yaml) )
     model = model_init(config['initial_conditions'])
     with logger_factory(logger_config(config)) as logger:
         model.run(logger)
---
+++ my_argparse.py
@@ -0,0 +1,7 @@
+import argparse
+
+def argparse_init():
+    parser = argparse.ArgumentParser(description = 'process yaml-config')
+    parser.add_argument('yaml', help='yaml file needed for configuration')
+    args = parser.parse_args()
+    return args
