1dfc7ca4dabe0ee8bc16f93fef1376f35a06c528
Mares.Barekzai <Mares.Barekzai@lmu.de>
2017-02-13 11:08:51 +0100

    Adding the math=(np, theano, sympy) option to boxmodel_functions

--- boxmodel_functions.py
+++ boxmodel_functions.py
@@ -4,31 +4,31 @@ from scipy.integrate import odeint
 
 
 # add second doc string, to the right location
-def saturation_pressure(T):
+def saturation_pressure(T, math=np):
   '''Return saturation pressure [Pa] over flat water surface from temperature [K]'''
   #T_min = 228.15
   #T_max = 333.15
   #assert (T_min < T < T_max), "{0} is out range ({1}, {2}) [K] for magnus approximation".format(T, T_min, T_max)
-  es = c.ES0 * np.exp(17.62 * (T - c.T0) / (243.12 + (T - c.T0)))
+  es = c.ES0 * math.exp(17.62 * (T - c.T0) / (243.12 + (T - c.T0)))
   return es
 
-def saturation_vapor(T, p):
+def saturation_vapor(T, p, math=np):
   '''Return saturation mixing ratio [kg kg-1] from T [K] and p [Pa]'''
-  es = saturation_pressure(T)
+  es = saturation_pressure(T, math=math)
   qvs = c.R_G / c.R_V * es / (p - es)
   return qvs
 
-def saturation_temperature(p, qv):
+def saturation_temperature(p, qv, math=np):
   '''Return saturation temperature [K] from p [Pa] and qv[kg kg-1]'''
   x  = qv * c.R_V * p / c.R_G / c.ES0
   c1 = 1. / c.T0  
   c2 = c.R_V / c.H_LAT 
-  T  = (c1 - np.log(x) * c2) ** (-1)
+  T  = (c1 - math.log(x) * c2) ** (-1)
   return T
 
-def saturation_adjustment(T, qv, qc, p):
+def saturation_adjustment(T, qv, qc, p, math=np):
   '''Return the state T [K], qv [kg kg-1], qv [kg kg-1] after saturation adjustment'''
-  delqc = np.maximum(0, qv - bf.saturation_vapor(T,p))
+  delqc = math.maximum(0, qv - bf.saturation_vapor(T,p))
   qc += delqc
   qv -= delqc
   T  += delqc * bc.H_LAT / bc.C_P
@@ -39,14 +39,14 @@ def relative_humidity(T,p,qv):
   r = qv / saturation_vapor(T,p)
   return r
 
-def cloud_water(N, r):
+def cloud_water(N, r, math=np):
   '''Return could water [kg kg-1] from number density N [m-3] and radius r [m]'''
-  qc = 4. / 3. * np.pi * r ** 3 * c.RHO_H2O / c.RHO_AIR * N
+  qc = 4. / 3. * math.pi * r ** 3 * c.RHO_H2O / c.RHO_AIR * N
   return qc
 
-def radius(qc, N):
+def radius(qc, N, math=np):
   '''Return mean radius in [m] from qc [kg kg-1] and N [m-3]'''
-  r = (3. / 4. / np.pi * qc * c.RHO_AIR / c.RHO_H2O / N) ** (1./3.)
+  r = (3. / 4. / math.pi * qc * c.RHO_AIR / c.RHO_H2O / N) ** (1./3.)
   return r
 
 def stefan_boltzmann_law(T):
@@ -61,10 +61,10 @@ def optical_thickness(qc):
   return tau
 
 #???CHECK???
-def thermal_radiation(T, qc, T_env=250):
+def thermal_radiation(T, qc, T_env=250, math=np):
   '''thermal radiation [units???] from cloud water mixing ratio [kg kg-1]'''
   tau   = optical_thickness(qc)
-  E_net = (stefan_boltzmann_law(T) - stefan_boltzmann_law(T_env))* (1 - np.exp(-tau))
+  E_net = (stefan_boltzmann_law(T) - stefan_boltzmann_law(T_env))* (1 - math.exp(-tau))
   return E_net
 
 #???CHECK???
@@ -88,8 +88,8 @@ def differential_growth_by_condensation_jacobian(r, t, E_net, es, T, S):
   r_new = - S / r ** 2 / (c1 * c.RHO_H2O)
   return r_new
 
-def condensation(T, p, qv, qc_sum, qc, particle_count, r_min, dt, radiation):
-    r_old = np.maximum(r_min, radius(qc, particle_count))
+def condensation(T, p, qv, qc_sum, qc, particle_count, r_min, dt, radiation, math=np):
+    r_old = math.maximum(r_min, radius(qc, particle_count))
     es = saturation_pressure(T)
     S = relative_humidity(T, p, qv) - 1
     if radiation:
@@ -97,7 +97,7 @@ def condensation(T, p, qv, qc_sum, qc, particle_count, r_min, dt, radiation):
     else:
         E = 0
     r_new = condensation_solver_linear(r_old, dt, E, es, T, S)
-    r_new = np.maximum(r_new, r_min)
+    r_new = math.maximum(r_new, r_min)
     delta_qc = cloud_water(particle_count, r_new) - qc
     delta_T = delta_qc * c.H_LAT / c.C_P
     delta_qv = -delta_qc
