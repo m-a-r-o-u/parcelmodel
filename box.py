c016577c5c2de5006bf4c25af9e0b9f480251eef
Mares.Barekzai <Mares.Barekzai@posteo.de>
2020-07-26 18:06:35 +0200

    Add yaml Loader

--- box.py
+++ box.py
@@ -1,4 +1,3 @@
-import sys
 import yaml
 from setup import model_init
 from logger import logger_factory
@@ -6,13 +5,16 @@ from config_tree import logger_config
 from my_argparse import argparse_init
 import executer
 
+
 def main():
     args = argparse_init()
-    config = yaml.load( open(args.yaml) )
-    model = model_init(config['initial_conditions'], executer.DISPATCHER[config['globals']['executer']])
+    config = yaml.load(open(args.yaml), Loader=yaml.FullLoader)
+    model = model_init(config['initial_conditions'],
+                       executer.DISPATCHER[config['globals']['executer']])
     with logger_factory(logger_config(config)) as logger:
         logger.inform(config['initial_conditions'])
         model.run(logger)
+
 
 if __name__ == "__main__":
     main()
